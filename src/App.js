4418ed7cd91a569e31716bf1ccf2dfc663230411
Sanchit Kashyap <sanchitkshyap@gmail.com>
2020-06-09 21:35:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css'
+import Game from './Game'
+
+
+export class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Conway's Game of Life</h1>
+                <Game />
+            </div>
+        )
+    }
+}
+
+export default App
